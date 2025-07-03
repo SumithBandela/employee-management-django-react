22cb650dbd07ad540586bebb12827b9cc0a9e97c
Sumith Bandela <sumithbandela@gmail.com>
2025-07-03 14:10:21 -0400

    commit

--- backend/backend/employees/models.py
+++ backend/backend/employees/models.py
@@ -9,7 +9,7 @@ class Employee(models.Model):
     phone = models.IntegerField()
     salary = models.DecimalField(max_digits=10,decimal_places=2)
     designation = models.CharField(max_length=255)
-    photo = models.FileField(upload_to='media/uploads/')
+    photo = models.FileField(upload_to='uploads/')
     joined_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
