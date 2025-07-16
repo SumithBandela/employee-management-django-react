a9f8411f08a2912d6c179b7da7588c9f0f77e310
Sumith Bandela <sumithbandela@gmail.com>
2025-07-16 15:06:52 -0400

    commit

--- backend/backend/employees/serializers.py
+++ backend/backend/employees/serializers.py
@@ -53,5 +53,5 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
 
 class ChangePasswordSerializer(serializers.Serializer):
-    old_password  = serializers.CharField(required=True)
-    new_password = serializers.CharField(required=True,min_length=6)
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True, min_length=6)
